24670cfb9bb80a6d260bf3790727f6536404a07b
Evert Arends <evert-arends@hotmail.nl>
2016-01-26 21:06:50 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    /*
+     * Register script, written by Evert Arends, in order for the myRemote project.
+     * 26/01/16
+     */
+    include_once('method.php');
+?>
+<html>
+    <head>
+        <title>Login to myRemote</title>
+    </head>
+    <body>
+        <form action="#" method="POST">
+            <input type="text" name="username" autocomplete="off"/><br />
+            <input type="password" name="password" autocomplete="off"/><br />
+            <input type="submit" value="Register" name="submit"/>
+        </form>
+    </body>
+</html>
+
+
+
